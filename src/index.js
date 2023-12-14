9e0395e23f834f4f3aa64fce1a7a797dce646313
Antoine P <antoine.plaineau@etudiant.univ-lr.fr>
2023-12-14 14:06:02 +0100

    create package

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/scss/main.scss";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+import Modal from "./components/Modal/Modal";
+
+export { Modal };
